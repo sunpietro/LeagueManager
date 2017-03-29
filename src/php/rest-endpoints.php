4c58d45b61743d2a09535a71bf54c0505fd2841e
Piotr Nalepa <piotr.nalepa@ez.no>
2017-03-29 22:24:49 +0200

    Fix #3 - Created an add player form

--- src/php/rest-endpoints.php
+++ src/php/rest-endpoints.php
@@ -1,5 +1,7 @@
 <?php
 
+// https://github.com/airesvsg/wp-rest-api-cache
+
 function expose_rest_endpoints() {
     global $wp_post_types;
     global $wp_taxonomies;
@@ -50,45 +52,5 @@ function expose_rest_endpoints() {
 }
 add_action('init', 'expose_rest_endpoints');
 
-function rest_api_create_event_info() {
-    register_rest_field('sp_event', 'event_info', array(
-           'get_callback' => 'get_event_info',
-           'schema' => null,
-        )
-    );
-}
-
-function get_event_info( $object ) {
-    $postId = $object['id'];
-    $transientName = 'event-info-' . $postId;
-
-    $current = get_transient($transientName);
-
-    if ($current) {
-        return $current;
-    }
-
-    $competition = wp_get_post_terms($postId, 'sp_league')[0];
-    $season = wp_get_post_terms($postId, 'sp_season')[0];
-    $homeTeam = get_post($object['teams'][0]);
-    $awayTeam = get_post($object['teams'][1]);
-
-    $info = array(
-        'competition' => $competition,
-        'teams' => array($homeTeam, $awayTeam),
-        'season' => $season,
-        'players' => array()
-    );
-
-    foreach ($object['players'] as $playerId) {
-        if ($playerId > 0) {
-            $info['players'][] = get_post($playerId);
-        }
-    }
-
-    set_transient($transientName, $info);
-
-    return $info;
-}
-
-add_action('rest_api_init', 'rest_api_create_event_info');
+require get_parent_theme_file_path('/inc/actions/event-actions.php');
+require get_parent_theme_file_path('/inc/actions/player-actions.php');
